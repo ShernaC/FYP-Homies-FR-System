22638270680aed4b176ab6e08369aca4ab128746
JeRry0t <130209144+JeRry0t@users.noreply.github.com>
2024-06-13 13:53:48 +0800

    Update accountUpdate.php

--- systemAdmin/accountUpdate.php
+++ systemAdmin/accountUpdate.php
@@ -95,7 +95,7 @@ $accounts = [
             </div>
             <div>
                 <label for="password" class="block text-sm font-medium text-gray-700">Password</label>
-                <input value="<?php $accounts["password"]; ?>" type="password" id="password" placeholder="System Admin, Business Owner only" class="mt-1 block w-full border border-gray-300 rounded-md shadow-sm py-2 px-3 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
+                <input value="<?php echo $accounts["password"]; ?>" type="password" id="password" placeholder="System Admin, Business Owner only" class="mt-1 block w-full border border-gray-300 rounded-md shadow-sm py-2 px-3 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
                 <span id="passwordError" class="error"></span>
             </div>
             <div class="text-right">
